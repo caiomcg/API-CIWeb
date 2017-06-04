c49bbb0de3d76b832af13870f502924cd4f8b202
caiomcg <caiomcg@gmail.com>
2017-06-04 15:08:29 -0300

    Remove logging from sequelize

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ var Sequelize = require("sequelize");
 
 var sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASS, {
     host: process.env.MYSQL_HOST,
-    dialect: "mysql"
+    dialect: "mysql",
+    logging: false
 });
 
 module.exports = db = {
