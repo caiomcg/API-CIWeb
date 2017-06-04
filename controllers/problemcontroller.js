f78b43adfcd1ae51862a4b18ed09e7cc99cf5b20
caiomcg <caiomcg@gmail.com>
2017-06-03 23:45:59 -0300

    Add delete

--- controllers/problemcontroller.js
+++ controllers/problemcontroller.js
@@ -14,23 +14,6 @@ exports.index = function (req, res, next) {
         }
     }, function (err) {
         res.status(500).send();
-    }).catch(function (err) {
-        res.status(500).send();
-    });
-};
-
-exports.find = function(req, res, next) {
-    db.report.findById(req.params.id).then(function (report) {
-        if (!!report) {
-            console.log("here3");
-            res.json(report.toJSON());
-        } else {
-            res.status(404).send();
-        }
-    }, function (err) {
-        res.status(500).send();
-    }).catch(function (err) {
-        res.status(500).send();
     });
 };
 
@@ -38,7 +21,46 @@ exports.add = function (req, res, next) {
     const body = _.pick(req.body, "name", "email", "title", "message", "state");
 
     db.report.create(body).then(function (report) {
-       res.json(report.toJSON());
+        res.json(report.toJSON());
+    }, function (err) {
+        res.status(500).send();
+    });
+};
+
+exports.find = function(req, res, next) {
+    db.report.findById(req.params.id).then(function (report) {
+        if (!!report) {
+            res.json(report.toJSON());
+        } else {
+            res.status(404).send();
+        }
+    }, function (err) {
+        res.status(500).send();
+    });
+};
+
+exports.update = function (req, res, next) {
+    const body = _.pick(req.body, "name", "email", "title", "message", "state");
+
+    db.report.find({where: {id: req.params.id}}).then(function (report) {
+        if (!!report) {
+            report.updateAttributes(body).then(function () {
+                res.json(body);
+            }).catch(function (err) {
+                console.log(err);
+                res.status(404).send();
+            })
+        }
+    });
+};
+
+exports.remove = function (req, res, next) {
+    db.report.destroy({where: {id: req.params.id}}).then(function (rows) {
+        if (rows === 0) {
+            res.sendStatus(404).json({error: "ID not found"});
+        } else {
+            res.sendStatus(204);
+        }
     }, function (err) {
         res.status(500).send();
     });
--- routes/problemroutes.js
+++ routes/problemroutes.js
@@ -7,7 +7,10 @@ var route   = express.Router();
 var problemController = require("../controllers/problemcontroller");
 
 route.get("/", problemController.index);
+route.post("/", problemController.add);
 route.get("/:id", problemController.find);
-route.put("/", problemController.add);
+route.put("/:id", problemController.update);
+route.delete("/:id", problemController.remove);
+
 
 module.exports = route;
