067fdbcc0cf922cbc93fe0ee7ef43035a6cbfc02
caiomcg <caiomcg@gmail.com>
2017-06-13 21:09:40 -0300

    Validate date

--- controllers/reservecontroller.js
+++ controllers/reservecontroller.js
@@ -28,14 +28,36 @@ exports.index = function (req, res, next) {
 
 exports.add = function (req, res, next) {
     const body = _.pick(req.body, "start", "finish", "user", "room_id", "always");
-    db.reserve.create(body).then(function (room) {
-        if (!!room) {
-            res.json(room.toJSON());
-        } else {
-            return error("Could not create the reserve", 400, next);
+
+
+    db.reserve.findAll({where: {room_id: body.room_id}}).then((reserves) => {
+        if (!!reserves) {
+            for (var i = 0; i < reserves.length; i++) {
+                console.log("Iterating");
+                const start  = new Date(reserves[i].start);
+                const finish = new Date(reserves[i].finish);
+
+                const newStart = new Date(body.start);
+                const newFinish = new Date(body.finish);
+
+                if (start.getTime() === newStart.getTime() || finish.getTime() === newFinish.getTime() || finish.getTime() === newStart.getTime() || start.getTime() === newFinish.getTime()) {
+                    return error("Room already reserved", 400, next);
+                }
+                if ((newStart > start && newStart < finish) || (newFinish > start && newFinish < finish)) {
+                    return error("Room already reserved", 400, next);
+                }
+            }
+
+            db.reserve.create(body).then(function (room) {
+                if (!!room) {
+                    res.json(room.toJSON());
+                } else {
+                    return error("Could not create the reserve", 400, next);
+                }
+            }, function () {
+                return error("Could not create the reserve", 400, next);
+            });
         }
-    }, function () {
-        return error("Could not create the reserve", 400, next);
     });
 };
 
@@ -53,19 +75,38 @@ exports.find = function(req, res, next) {
 };
 
 exports.update = function (req, res, next) {
-    const body = _.pick(req.body, "start", "finish", "user", "room_id", "always");
+    const body = _.pick(req.body, "start", "finish", "user", "always");
 
     db.reserve.find({where: {id: req.params.id}}).then(function (room) {
         if (!!room) {
-            room.updateAttributes(body).then(function () {
-                if (!Object.keys(body).length) {
-                    return error("Could not update with the desired information", 400,  next);
-                } else {
-                    res.json(body);
+            db.reserve.findAll({where: {room_id: room.room_id}}).then((reserves) => {
+                if (!!reserves) {
+                    for (var i = 0; i < reserves.length; i++) {
+                        const start = new Date(reserves[i].start);
+                        const finish = new Date(reserves[i].finish);
+
+                        const newStart = new Date(body.start);
+                        const newFinish = new Date(body.finish);
+
+                        if (start.getTime() === newStart.getTime() || finish.getTime() === newFinish.getTime() || finish.getTime() === newStart.getTime() || start.getTime() === newFinish.getTime()) {
+                            return error("Another event starts or finishes at the same time", 400, next);
+                        }
+                        if ((newStart > start && newStart < finish) || (newFinish > start && newFinish < finish)) {
+                            return error("Cannot create a reserve between another", 400, next);
+                        }
+                    }
+
+                    room.updateAttributes(body).then(function () {
+                        if (!Object.keys(body).length) {
+                            return error("Could not update with the desired information", 400,  next);
+                        } else {
+                            res.json(body);
+                        }
+                    }).catch(function (err) {
+                        return error("Could not update reserve with ID " + req.params.id, 404, next);
+                    })
                 }
-            }).catch(function (err) {
-                return error("Could not update reserve with ID " + req.params.id, 404, next);
-            })
+            });
         }
     }, function (err) {
         return error("An error occurred while fetching from the database", 500, next);
